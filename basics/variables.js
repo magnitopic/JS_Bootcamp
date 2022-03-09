bb4e5f3a0ea63edf2c2fe37c0a4fcf43a3e551b7
Magnitopic <magnitopic@gmail.com>
2022-03-09 23:47:30 +0100

    Variables

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+// 1. you can't define vars more than once, you change your value
+
+let petName = "Hall";
+petName = "Spot";
+
+console.log(petName);
+
+// 2. There are rules related to variable names
+let test_ = 2;
+let result = 3 + 4;
+
+// 3. Variable names cannot be reserved keywords
+// let let = 12
