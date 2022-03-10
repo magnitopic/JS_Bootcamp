ab8a610a5451d81fc6087a5570ac43ab69c77487
Magnitopic <magnitopic@gmail.com>
2022-03-10 01:10:23 +0100

    booleans

---
+++ basics/boolean.js
@@ -0,0 +1,29 @@
+// === - equality operator
+// != - no equal operator
+// < - less than operator
+// > - greater than operator
+// <= - less than or equal to operator
+// >= - greater than or equal to operator
+
+let temp = 32;
+let isFreezing = temp <= 32;
+
+console.log(isFreezing);
+
+// Challange
+
+
+let ageCheker= function(age) {
+	if (age <= 7) {
+		console.log("You are a child");
+	} else if (age >= 65) {
+		console.log("You are a senior");
+	}else{
+		console.log("You are an adult");
+	}
+}
+
+ageCheker(69)
+ageCheker(5)
+ageCheker(43)
+
