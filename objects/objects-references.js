5fa3b995257d02ec43a8e611ef9fec5f37180b94
Magnitopic <magnitopic@gmail.com>
2022-03-12 01:18:29 +0100

    Challenge for objects references

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -10,3 +10,26 @@ let addExpense = function (account, amount) {
 
 addExpense(myAccount, 2.5);
 console.log(myAccount);
+
+// Challenge - addIncome
+let addIncome = function (account, amount) {
+	account.income = account.income + amount;
+};
+// Challenge - resetAccount
+let resetAccount = function (account) {
+	account.expenses = 0;
+	account.income = 0;
+};
+// Challenge - getAccountSummary
+let getAccountSummary = function (account) {
+	console.log(
+		`${account.name}'s has ${account.expenses}€ in expenses and ${account.income}€ in income`
+	);
+};
+
+addIncome(myAccount, 1000);
+addExpense(myAccount, 547);
+addExpense(myAccount, 85);
+getAccountSummary(myAccount);
+resetAccount(myAccount);
+getAccountSummary(myAccount);
