304592b992f754eabc825a7304aef6b447fb2bec
magnitopic <magnitopic@gmail.com>
2022-03-13 19:20:00 +0100

    Array basics

---
+++ arrays/notes.js
@@ -0,0 +1,6 @@
+const notes = ["Note1", "Note2", "Note3"];
+
+console.log(notes.length);
+console.log(notes[0]);
+console.log(notes[2]);
+console.log(notes[notes.length-1]);
---
+++ arrays/todo.js
@@ -0,0 +1,5 @@
+let toDos = ["Walk", "Work", "Coffe", "Sleep"];
+
+console.log(`You have ${toDos.length} todos`);
+console.log(`Todo: ${toDos[toDos.length - 1]}`);
+console.log(`Todo: ${toDos[toDos.length - 2]}`);
