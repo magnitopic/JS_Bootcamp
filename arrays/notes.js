78ac685b26658d193b6cf3ec9c145028ee47f29d
magnitopic <magnitopic@gmail.com>
2022-03-21 00:03:19 +0100

    For loop

--- arrays/notes.js
+++ arrays/notes.js
@@ -12,3 +12,11 @@ notes[2] = "New note 3";
 
 console.log(notes.length);
 console.log(notes);
+
+for (let count = 0; count >= 0; count--) {
+	console.log(count);
+}
+
+for (let count = 0; count < notes.length; count++) {
+	console.log(notes[count]);
+}
--- arrays/todo.js
+++ arrays/todo.js
@@ -5,6 +5,10 @@ todos.push("Exercice");
 todos.shift();
 
 console.log(`You have ${todos.length} todos`);
-todos.forEach((item,index) => {
+todos.forEach((item, index) => {
 	console.log(`${index}. ${item}`);
 });
+
+for (let i = 0; i < todos.length; i++) {
+	console.log(todos[i]);
+}
