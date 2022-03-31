c3d4008c05f01290d3dae6b2838881db07cee1f0
Magnitopic <magnitopic@gmail.com>
2022-04-01 00:32:17 +0200

    DOM

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,11 @@
-console.log("yes");
+/* const p = document.querySelector("p");
+p.remove(); */
+
+// Query all and remove
+const ps = document.querySelectorAll("p");
+
+ps.forEach((p) => {
+	p.textContent="*****"
+	// console.log(p.textContent);
+	// p.remove();
+});
