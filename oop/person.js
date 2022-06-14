5ee3a0aa7046a4ac98e9c3d9ba6254f461ecb861
Magnitopic <magnitopic@gmail.com>
2022-06-14 12:02:00 +0200

    Methods for objects

--- oop/person.js
+++ oop/person.js
@@ -1,14 +1,28 @@
-const Person = function (firstName, lastName, age) {
+const Person = function (firstName, lastName, age, likes = []) {
 	this.firstName = firstName;
 	this.lastName = lastName;
 	this.age = age;
+	this.likes = likes;
 };
 
 Person.prototype.getBio = function () {
-	return `${this.firstName} is ${this.age}`;
+	let bio = `${this.firstName} is ${this.age}.`;
+
+	this.likes.forEach((like) => {
+		bio += ` ${this.firstName} likes ${like}.`;
+	});
+
+	return bio;
 };
 
-const me = new Person("Mag", "nitopic", "18");
+Person.prototype.setName = function (fullName) {
+	const names = fullName.split(" ");
+	this.firstName = names[0];
+	this.lastName = names[1];
+};
+
+const me = new Person("Mag", "nitopic", 18, ["Teaching", "Biking"]);
+me.setName("Alex Turner");
 console.log(me.getBio());
 
 const person2 = new Person("Clancey", "Turner", 51);
