2636e0ec22f61f460735f9f3db7e5e3ead417683
Magnitopic <magnitopic@gmail.com>
2022-06-20 12:50:35 +0200

    Hangman

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,12 +1,25 @@
 // challenge area
 
 const Hangman = function (word, guesses) {
-	this.word = word;
+	this.word = word.toLowerCase().split("");
 	this.guesses = guesses;
+	this.guessedLetters = ["a", "e"];
+};
+
+Hangman.prototype.getPuzzel = function () {
+	let puzzel = "";
+	this.word.forEach((letter) => {
+		if (letter === " " || this.guessedLetters.includes(letter)) {
+			puzzel += letter;
+		} else {
+			puzzel += "*";
+		}
+	});
+	return puzzel;
 };
 
 const game1 = new Hangman("Cat", 2);
-console.log(game1);
+console.log(game1.getPuzzel());
 
-const game2 = new Hangman("Bridge", 4);
-console.log(game2);
+const game2 = new Hangman("Hello There", 4);
+console.log(game2.getPuzzel());
